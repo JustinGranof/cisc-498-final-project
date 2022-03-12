129ad2f30ff6ab86ac5d5a7f9b7a006b74df3d8b
Justin Granofsky <justin.granofsky@gmail.com>
2022-03-11 23:28:52 -0500

    Added in a modal

---
+++ frontend/src/routes/login/Modal.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+// CSS imports
+import "./modal.css";
+
+export default function Modal(props) {
+  const show = props.show;
+
+  // component did mount
+  useEffect(() => {
+    // disable scrolling
+    window.document.body.style.overflowY = show ? "hidden" : "auto";
+  }, [show]);
+
+  if (!show) return null;
+
+  return (
+    <React.Fragment>
+      <div onClick={() => props.setShow(false)} className="dim" />
+      <div className="modal-container">{props.children}</div>
+    </React.Fragment>
+  );
+}
