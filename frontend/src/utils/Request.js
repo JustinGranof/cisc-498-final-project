14fd7b1c35e584814150fc265105f6eecbcc6e73
Justin Granofsky <justin.granofsky@gmail.com>
2022-03-15 12:18:38 -0400

    Added some comments

--- frontend/src/utils/Request.js
+++ frontend/src/utils/Request.js
@@ -1,3 +1,11 @@
+/**
+ * Make an http request to the backend NodeJS server
+ * @param {String} method http request type
+ * @param {String} path the path of the http request
+ * @param {Object} body the post request body
+ * @param {Object} headers the headers for http request
+ * @returns the response from the http request in json format
+ */
 export default async function request(
   method = "GET",
   path = "",
