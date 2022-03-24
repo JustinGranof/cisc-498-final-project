0a65ff8e236be6a4fc512d2d1f1c661745379595
Justin Granofsky <justin.granofsky@gmail.com>
2022-03-24 12:43:27 -0400

    Started working on new user methods

--- backend/mongo_classes/userClass.js
+++ backend/mongo_classes/userClass.js
@@ -1,59 +1,55 @@
 const mongoDbClass = require("./dbClass");
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
 
 class Users extends mongoDbClass {
-    constructor(){
-        super()
+  constructor() {
+    super();
+  }
+
+  async login(username, password) {
+    // Create a reference to the users collection
+    const UsersCollection = this.db.collection("Users");
+
+    // Define the query
+    const query = { email: username };
+
+    const user = await UsersCollection.findOne(query, {});
+    console.log(user);
+
+    if (user == null) {
+      return { error: true, body: "Invalid Credentials." };
+    } else {
+      //Compares the password to the hash in the database to see if it is correct
+      if (bcrypt.compareSync(password, user.password)) {
+        //Define what is returned. i.e. don't return password
+        var returnData = {
+          email: user.email,
+          level: user.level,
+        };
+        return { error: false, body: returnData };
+      } else {
+        return { error: true, body: "Invalid Crendentials." };
+      }
     }
+  }
 
-    async login(username, password){
-        const UsersCollection = this.db.collection("Users");
-
-        const query = { email: username };
-        const options = {};
-
-        const user = await UsersCollection.findOne(query, options);
-        console.log(user);
-
-        if (user == null){
-            return {'error': true, 'body': 'Invalid Credentials.'};
-        }
-        else {
-            //Compares the password to the hash in the database to see if it is correct
-            if (bcrypt.compareSync(password, user.password)){
-                //Define what is returned. i.e. don't return password
-                var returnData = {
-                    'email': user.email,
-                    'level': user.level
-                }
-                return {'error': false, 'body': returnData};
-            }
-            else {
-                return {'error': true, 'body': 'Invalid Crendentials.'};
-            }
-            
-          
-        }
-
-    }
-
-    createUser(doc){
-        const UsersCollection = this.db.collection("Users");
-        /*const doc = {
+  createUser(doc) {
+    const UsersCollection = this.db.collection("Users");
+    /*const doc = {
             name: "Test Test",
             email: "test@test.com",
         }*/
-        //email password email is unique
-       // try{
-            const result = UsersCollection.insertOne(doc);
-            return true;
-        // }
-        // catch {
-        //     return false;
-        // }
-
-    }
+    //email password email is unique
+    // try{
+    const result = UsersCollection.insertOne(doc);
+    return true;
+    // }
+    // catch {
+    //     return false;
+    // }
+  }
 }
 
+async function getAllAdmins() {}
 
 module.exports = Users;
