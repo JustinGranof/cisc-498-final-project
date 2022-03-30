4c8ce31446c958cccdb3135cdc57569921ba19ae
Justin Granofsky <justin.granofsky@gmail.com>
2022-03-30 19:13:45 -0400

    Worked on routing

--- frontend/src/routes/classes/class.js
+++ frontend/src/routes/classes/class.js
@@ -1,151 +1,179 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import "./class.css";
 import { RiDeleteBinFill } from "react-icons/ri";
-import Switch from 'react-input-switch';
+import Switch from "react-input-switch";
 import request from "../../utils/Request";
 
 export default function Class() {
+  const { tripID } = useParams();
+  const navigate = useNavigate();
 
-    var tripId = "623cac53c071a74d9056f69a";
+  const [query, setQuery] = useState("");
+  const [value, setValue] = useState("yes");
+  const location = useLocation();
+  const [search, setSearch] = useState("");
+  const [students, setStudents] = useState(undefined);
 
-    const [query, setQuery] = useState("")
-    const [value, setValue] = useState('yes');
-    const location = useLocation();
-    const [search, setSearch] = useState("");
-    const [students, setStudents] = useState(undefined);
+  useEffect(() => {
+    getStudents();
+  }, []);
 
-    useEffect(() => {
-       getStudents();
-    }, []);
-
-    async function getStudents() {
-        let data = await request("POST", "trip/students/get", {tripID: tripId}, true);
-    
-        if (!data.success) {
-          alert("Error getting class information.");
-          return;
-        }
-        console.log(data.body);
-    
-        setStudents(data.body);
-    }
-
-
-    // get data for {location.state.name}
-    // example data
-    // const students = [
-    //     { "id": 1, "firstName": "Kim", "lastName": "Kardashian", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    //     { "id": 2, "firstName": "Pete", "lastName": "Davidson", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    //     { "id:": 3, "firstName": "Kanye", "lastName": "West", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    //     { "id": 4, "firstName": "North", "lastName": "West", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    //     { "id": 5, "firstName": "Miley", "lastName": "Cyrus", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    //     { "id": 6, "firstName": "Asap", "lastName": "Rocky", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    //     { "id": 7, "firstName": "Selena", "lastName": "Gomez", "email": "abc@gmail.com", "phone": "613-849-3928" },
-    // ]
-
-    async function deleteStudent(email) {
-        let data = await request(
-            "POST",
-            "trip/student/delete",
-            { email: email },
-            true
-        );
-        // if (data) {
-        //   //get Data
-        // }
-    }
-
-    async function getJoinLink() {
-        return;
-    }
-
-    function studentList() {
-        if (students == undefined){
-            return "loading";
-        }
-        return students.map((student, index) => {
-            const { id, name, email, phone } = student;
-
-
-            if (!name.includes(search.toLowerCase())) return;
-            return (
-                <tr key={id}>
-                    <td>{name}</td>
-                    <td>{email}</td>
-                    <td>{phone}</td>
-                    <td>
-                        <button onClick={() => {
-                            if (
-                                window.confirm(
-                                    "Are you sure you want to delete " +
-                                    name +
-                                    "?"
-                                )
-                            ) deleteStudent(email)
-                        }}
-                            className="delete-btn">
-                            <RiDeleteBinFill></RiDeleteBinFill>
-                        </button>
-                    </td>
-                </tr>
-            )
-        })
-    }
-
-    return (
-        <div className="classes-container">
-            <div className="nav">
-                <p className="logout">
-                    <Link to="/classes">Back to classes</Link>
-                </p>
-                <p className="logout">
-                    <Link to="/logout">Logout</Link>
-                </p>
-            </div>
-
-            <h1>{location.state.name}</h1>
-
-            <div className="header-bar">
-                <button onClick={() => { getJoinLink() }} className="join-btn">Get Student Join Link</button>
-                <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-                    <p style={{ margin: 10 }}>Allow new students to join</p>
-                    <Switch styles={{
-                        track: {
-                            backgroundColor: 'gray'
-                        },
-                        trackChecked: {
-                            backgroundColor: '#20E00F'
-                        },
-                        button: {
-                            backgroundColor: '#F2F2F2'
-                        },
-                        buttonChecked: {
-                            backgroundColor: '#F2F2F2'
-                        }
-                    }} on="yes" off="no" value={value} onChange={setValue} />
-                </div>
-            </div>
-
-            <div className="search-container">
-                <label htmlFor="search">Search</label>
-                <br />
-                <input
-                    autoComplete="off"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    id="search"
-                    placeholder="Enter a student's name"
-                    className="search-input"
-                />
-            </div>
-
-            <div className="table-container">
-                <table className="student-table" style={{ marginTop: 20 }}>
-                    <tbody>{studentList()}</tbody>
-                </table>
-            </div>
-
-        </div>
+  async function getStudents() {
+    let data = await request(
+      "POST",
+      "trip/students/get",
+      { tripID: tripID },
+      true
     );
+
+    if (!data.success) {
+      alert("Error getting class information.");
+      return;
+    }
+    console.log(data.body);
+
+    setStudents(data.body);
+  }
+
+  // get data for {location.state.name}
+  // example data
+  // const students = [
+  //     { "id": 1, "firstName": "Kim", "lastName": "Kardashian", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  //     { "id": 2, "firstName": "Pete", "lastName": "Davidson", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  //     { "id:": 3, "firstName": "Kanye", "lastName": "West", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  //     { "id": 4, "firstName": "North", "lastName": "West", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  //     { "id": 5, "firstName": "Miley", "lastName": "Cyrus", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  //     { "id": 6, "firstName": "Asap", "lastName": "Rocky", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  //     { "id": 7, "firstName": "Selena", "lastName": "Gomez", "email": "abc@gmail.com", "phone": "613-849-3928" },
+  // ]
+
+  async function deleteStudent(email) {
+    let data = await request(
+      "POST",
+      "trip/student/delete",
+      { email: email },
+      true
+    );
+    // if (data) {
+    //   //get Data
+    // }
+  }
+
+  async function getJoinLink() {
+    return;
+  }
+
+  function studentList() {
+    if (students == undefined) {
+      return "loading";
+    }
+    return students.map((student, index) => {
+      const { id, name, email, phone } = student;
+
+      if (!name.includes(search.toLowerCase())) return;
+      return (
+        <tr
+          onClick={() => {
+            navigate("/classes/" + tripID + "/" + id);
+          }}
+          className="class-student-row"
+          key={id}
+        >
+          <td>{name}</td>
+          <td>{email}</td>
+          <td>{phone}</td>
+          <td>
+            <button
+              onClick={() => {
+                if (
+                  window.confirm(
+                    "Are you sure you want to delete " + name + "?"
+                  )
+                )
+                  deleteStudent(email);
+              }}
+              className="delete-btn"
+            >
+              <RiDeleteBinFill></RiDeleteBinFill>
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  return (
+    <div className="classes-container">
+      <div className="nav">
+        <p className="logout">
+          <Link to="/classes">Back to classes</Link>
+        </p>
+        <p className="logout">
+          <Link to="/logout">Logout</Link>
+        </p>
+      </div>
+
+      <h1>{location.state.name}</h1>
+
+      <div className="header-bar">
+        <button
+          onClick={() => {
+            getJoinLink();
+          }}
+          className="join-btn"
+        >
+          Get Student Join Link
+        </button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <p style={{ margin: 10 }}>Allow new students to join</p>
+          <Switch
+            styles={{
+              track: {
+                backgroundColor: "gray",
+              },
+              trackChecked: {
+                backgroundColor: "#20E00F",
+              },
+              button: {
+                backgroundColor: "#F2F2F2",
+              },
+              buttonChecked: {
+                backgroundColor: "#F2F2F2",
+              },
+            }}
+            on="yes"
+            off="no"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+
+      <div className="search-container">
+        <label htmlFor="search">Search</label>
+        <br />
+        <input
+          autoComplete="off"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          id="search"
+          placeholder="Enter a student's name"
+          className="search-input"
+        />
+      </div>
+
+      <div className="table-container">
+        <table className="student-table" style={{ marginTop: 20 }}>
+          <tbody>{studentList()}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- frontend/src/routes/classes/classesList.js
+++ frontend/src/routes/classes/classesList.js
@@ -7,152 +7,147 @@ import request from "../../utils/Request";
 import { RiDeleteBinFill } from "react-icons/ri";
 
 export default function ClassList() {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const [open, setOpen] = useState(false);
-    const [name, setName] = useState("");
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
+  useEffect(() => {
+    getClasses();
+  }, []);
 
-    useEffect(() => {
-        getClasses();
-     }, []);
- 
+  /* get classes data */
+  const [classes, setClasses] = useState(undefined);
 
-    /* get classes data */
-    const [classes, setClasses] = useState(undefined);
+  async function getClasses() {
+    let data = await request("POST", "trip/get", {}, true);
 
-    async function getClasses() {
-        let data = await request("POST", "trip/get", {}, true);
-    
-        if (!data.success) {
-          alert("Error getting class information.");
-          return;
-        }
-        console.log(data.body);
-    
-        setClasses(data.body);
+    if (!data.success) {
+      alert("Error getting class information.");
+      return;
     }
+    console.log(data.body);
 
-    const toClass = (className) => {
-        navigate('/class', { state: { name: className } });
+    setClasses(data.body);
+  }
+
+  const toClass = (tripID, className) => {
+    navigate("/classes/" + tripID, { state: { name: className } });
+  };
+
+  async function createClass() {
+    if (!name) return;
+    let data = await request("POST", "class/create", { name: name }, true);
+    // check for unique class name
+    if (data && data.success) {
+      setOpen(false);
+    } else {
+      setError(data.body);
     }
+  }
 
-    async function createClass() {
+  async function deleteClass(name) {
+    let data = await request("POST", "class/delete", { name: name }, true);
+    // if (data) {
+    //   //get Data
+    // }
+  }
 
-        if (!name) return;
-        let data = await request(
-            "POST",
-            "class/create",
-            { name: name },
-            true
-        );
-        // check for unique class name
-        if (data && data.success) {
-            setOpen(false);
-        } else {
-            setError(data.body);
-        }
-    }
+  // Display classes
+  return (
+    <>
+      <Modal show={open} setShow={setOpen}>
+        <h2>Create a new class</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            createClass();
+          }}
+        >
+          <label>What would you like to name this class?</label>
+          <br />
+          <input
+            className="create-admin-input"
+            value={name}
+            placeholder="Ex. GEOL 101"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          {error && (
+            <p style={{ marginTop: "25px", color: "red" }}>Error: {error}</p>
+          )}
+          <div>
+            <button
+              type={"submit"}
+              style={{ backgroundColor: "#00305E" }}
+              className="create-admin-btn"
+            >
+              Create
+            </button>
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+              className="create-admin-btn"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
 
-    async function deleteClass(name) {
-        let data = await request(
-            "POST",
-            "class/delete",
-            { name: name },
-            true
-        );
-        // if (data) {
-        //   //get Data
-        // }
-    }
-
-    // Display classes
-    return (
-        <>
-            <Modal show={open} setShow={setOpen}>
-                <h2>Create a new class</h2>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        createClass();
-                    }}
-                >
-                    <label>What would you like to name this class?</label>
-                    <br />
-                    <input
-                        className="create-admin-input"
-                        value={name}
-                        placeholder="Ex. GEOL 101"
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <br />
-                    {error && (
-                        <p style={{ marginTop: "25px", color: "red" }}>Error: {error}</p>
-                    )}
-                    <div>
-                        <button
-                            type={"submit"}
-                            style={{ backgroundColor: "#00305E" }}
-                            className="create-admin-btn"
-                        >
-                            Create
-                        </button>
-                        <button
-                            onClick={() => {
-                                setOpen(false);
-                            }}
-                            className="create-admin-btn"
-                        >
-                            Cancel
-                        </button>
-                    </div>
-                </form>
-            </Modal>
-
-            <div className="classes-container">
-                <p className="logout">
-                    <Link to="/logout">Logout</Link>
-                </p>
-                <div className="header-container">
-                    <h1>Classes</h1>
-                    <button className="create-btn"
-                        onClick={() => {
-                            setOpen(true);
-                            setError("");
-                            setName("");
-                        }}>
-                        <div className="create-btn-container">
-                            <AiOutlinePlusCircle style={{ margin: 5 }}></AiOutlinePlusCircle>
-                            <p>Create new</p>
-                        </div>
-                    </button>
-                </div>
-
-                {classes && classes.map((item, index) => (
-                    <button className="class-btn">
-                        <div className="delete-btn-container">
-                            <button className="delete-btn">
-                                <RiDeleteBinFill className="delete-btn"
-                                    onClick={(e) => {
-                                        if (
-                                            window.confirm(
-                                                "Are you sure you want to delete " +
-                                                item.name +
-                                                "?"
-                                            )
-                                        )
-                                            deleteClass(item.name);
-                                    }}></RiDeleteBinFill>
-                            </button>
-                        </div>
-
-                        <div onClick={() => { toClass(item.name) }} className="link" >
-                            <p className="name">{item.name}</p>
-                            <p className="size">{item.numStudents} students</p>
-                        </div>
-                    </button>
-                ))}
+      <div className="classes-container">
+        <p className="logout">
+          <Link to="/logout">Logout</Link>
+        </p>
+        <div className="header-container">
+          <h1>Classes</h1>
+          <button
+            className="create-btn"
+            onClick={() => {
+              setOpen(true);
+              setError("");
+              setName("");
+            }}
+          >
+            <div className="create-btn-container">
+              <AiOutlinePlusCircle style={{ margin: 5 }}></AiOutlinePlusCircle>
+              <p>Create new</p>
             </div>
-        </>
-    );
+          </button>
+        </div>
+
+        {classes &&
+          classes.map((item, index) => (
+            <button className="class-btn">
+              <div className="delete-btn-container">
+                <button className="delete-btn">
+                  <RiDeleteBinFill
+                    className="delete-btn"
+                    onClick={(e) => {
+                      if (
+                        window.confirm(
+                          "Are you sure you want to delete " + item.name + "?"
+                        )
+                      )
+                        deleteClass(item.name);
+                    }}
+                  ></RiDeleteBinFill>
+                </button>
+              </div>
+
+              <div
+                onClick={() => {
+                  toClass(item._id, item.name);
+                }}
+                className="link"
+              >
+                <p className="name">{item.name}</p>
+                <p className="size">{item.numStudents} students</p>
+              </div>
+            </button>
+          ))}
+      </div>
+    </>
+  );
 }
